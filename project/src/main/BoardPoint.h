946bd437c93e01960c4870d628a3fbcc5b6a1bf8
Ofer Doron <oferdoron@localhost.localdomain>
2015-04-01 20:25:11 +0300

    Evaluation test:w

--- project/src/main/BoardPoint.h
+++ project/src/main/BoardPoint.h
@@ -1,13 +1,19 @@
 #ifndef BOARD_POINT_H_
 #define BOARD_POINT_H_
 
+#include "Constants.h"
+#include "stdio.h"
+
 typedef struct board_point {
 	int row;
 	int col;
+	int dist;
 } BoardPoint;
 
 int isAdjacent(BoardPoint point1, BoardPoint point2);
 
-float calcDistance(BoardPoint point1, BoardPoint point2);
+float calcOptDistance(BoardPoint point1, BoardPoint point2);
+
+float calcRealDistance(char **board, BoardPoint origin, BoardPoint destination, int isMouse);
 
 #endif /* BOARD_POINT_H_ */
--- project/src/main/Evaluation.c
+++ project/src/main/Evaluation.c
@@ -5,18 +5,25 @@
 // TODO remove
 #include <stdio.h>
 
+#define INT_MIN -32768
+#define INT_MAX 32769
+
 // Mouse is min player, cat is max player
 int getScoreForState(char **board, BoardPoint catPoint, BoardPoint mousePoint, BoardPoint cheesePoint) {
-	float catToMouse, mouseToCheese;
-	catToMouse = calcDistance(catPoint, mousePoint);
-	mouseToCheese = calcDistance(mousePoint, cheesePoint);
-	
-	if (catToMouse == 1.0) {
-		return MAX_EVALUATION;
-	} else if (mouseToCheese == 1.0) {
-		return MIN_EVALUATION;
-	} else {
-		return (int) 10 * mouseToCheese - 10 * catToMouse;
+	if(isAdjacent(mousePoint, catPoint)){
+		return INT_MAX;
+	} else if(isAdjacent(mousePoint, cheesePoint)){
+		return INT_MIN;
 	}
+	/*float catToMouseReal, mouseToCheeseReal, catToMouseOpt, mouseToCheeseOpt, catToCheeseReal, mouse, cat, catAndMouse;
+	catToMouseReal = calcRealDistance(board, catPoint, mousePoint,0);
+	mouseToCheeseReal =  calcRealDistance(board, mousePoint, cheesePoint, 1);
+	catToCheeseReal =  calcRealDistance(board, catPoint, cheesePoint, 0);
+	catToMouseOpt = calcOptDistance(catPoint, mousePoint);
+	mouseToCheeseOpt = calcOptDistance(mousePoint, cheesePoint);
+	mouse = mouseToCheeseReal - mouseToCheeseOpt;
+	cat = catToMouseReal - catToMouseOpt;
+	catAndMouse = mouseToCheeseReal - catToMouseReal - catToCheeseReal;
+	return mouse - cat + 3*catAndMouse;*/
 }
 
