70f821821bba8b3bdcef39536921143bd88d5808
Ofer Doron <oferdoron@localhost.localdomain>
2015-05-08 15:34:24 +0300

    fdsn,m

--- project/src/main/GUI/UITree.h
+++ project/src/main/GUI/UITree.h
@@ -8,7 +8,11 @@
 #include "Panel.h"
 #include "Label.h"
 
+/* This function receives a Widget pointer and draws the widget and all of his descendants in The UITree. */
 int drawUITree(Widget *widget);
+
+/* This function receives two Uint16 x, y and a widget pointer and returns a pointer to the widget which the
+ x and y fall in his territory. */
 Widget *findWidgetFromTree(Uint16 x, Uint16 y, Widget *widget);
 
 #endif
